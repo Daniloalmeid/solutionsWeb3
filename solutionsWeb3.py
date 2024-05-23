3d6dbe8355470158305d793ab106d8b643c64b46
Danilo Almeida <danilotecteatro@yahoo.com.br>
2024-05-22 21:35:41 -0300

    Update solutionsWeb3.py

--- solutionsWeb3.py
+++ solutionsWeb3.py
@@ -1,2 +1,7 @@
 mensagem = {'m1': {'m2': 'Olá Mundo'}}
 print(mensagem['m1']['m2'])
+
+numeros = [21, 5, 34, 8, 16, 7, 3]
+soma_pares = sum(map(lambda n: n if n % 2 == 0 else 0, numeros))
+soma_impares = sum(map(lambda n: n if n % 2 != 0 else 0, numeros))
+print(f'A soma dos valores pares é {soma_pares} e dos ímpares é {soma_impares}')
